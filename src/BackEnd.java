fbc2fefb799e4969ca9a4edb78ca019d9376c229
ajboss98 <aj280598@gmail.com>
2015-04-07 10:33:41 -0500

    updated backend
    lols

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,4 +1,20 @@
+import java.util.List;
+
 
 public class BackEnd {
 
+	private List<String> usernames;
+	private List<String> messages;
+	
+	 public BackEnd(){
+		 usernames = null;
+	 }
+	 
+	 public List<String> getUsernames(){
+		 return usernames;
+	 }
+	 public void addUser(String username){
+		 usernames.add(username);
+	 }
+	 
 }
