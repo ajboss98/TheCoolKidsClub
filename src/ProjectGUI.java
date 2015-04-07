32d830bc00704c26b1423883c25a98d5c52c75c2
Shaharyar Ali <cherrypie.197@gmail.com>
2015-04-07 10:34:16 -0500

    Updated GUI

--- src/ProjectGUI.java
+++ src/ProjectGUI.java
@@ -1,4 +1,23 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 
 public class ProjectGUI {
 
+	public ProjectGUI()
+	{
+		JFrame frame = new JFrame("");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Box 
+	}
+	public static void main(String[] args)
+	{
+		{	
+			SwingUtilities.invokeLater(new Runnable() 
+			{
+				public void run() { new ProjectGUI(); }
+			});
+		}
+	}
 }
