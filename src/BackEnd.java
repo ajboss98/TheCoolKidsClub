21b1c11c634c2066563fc1ecaea46db98385e247
ajboss98 <aj280598@gmail.com>
2015-04-09 10:50:13 -0500

    lols backend DONE

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,9 +1,10 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
 
 
 public class BackEnd {
@@ -15,15 +16,38 @@ public class BackEnd {
 	 }
 	 
 	 public List<String> getMessages(String username){
-		 return null;
+		 Encoder temp = new Encoder(username);
+		 return temp.read();
 	 }
 	 public void addMessage(String username, String msg){
 		 Encoder temp = new Encoder(username);
-		 
+		 temp.write(username+": "+msg);
+		 try{
+		 FileWriter filewriter = new FileWriter(userlist, true);
+		 BufferedWriter bufferedwriter = new BufferedWriter(filewriter);
+		 bufferedwriter.write(username+"\\n");
+		 bufferedwriter.close();
+		 }
+		 catch(Exception e){
+				e.printStackTrace();
+			}
 	 }
 	 public List<String> getUsers()
 	 {		
-		return null;		
+		Scanner scan = null;
+		List<String> toret = null;
+		try {
+			scan = new Scanner(userlist);
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while(scan.hasNextLine()){
+			String input = scan.nextLine();
+			toret.add(input);
+		}
+		scan.close();
+		return toret;
 	  }
 	 
 }
