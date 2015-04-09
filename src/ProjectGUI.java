d9774b564b7af91578ad5cc8d803110a3c8f1f23
Shaharyar Ali <cherrypie.197@gmail.com>
2015-04-09 10:27:45 -0500

    Updated GUI

--- src/ProjectGUI.java
+++ src/ProjectGUI.java
@@ -1,15 +1,33 @@
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 
 public class ProjectGUI {
 
+	private List<String> usernames;
+	private List<String> messages;
+	private String message;
+	private BackEnd daConvos;
 	public ProjectGUI()
 	{
-		JFrame frame = new JFrame("");
+		JFrame frame = new JFrame("deConvos");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Box 
+		Box usernameBox = Box.createVerticalBox();
+		Box searchBox = Box.createVerticalBox();
+		daConvos = new BackEnd();
+		//usernameBox
+		
+		
+		//searchBox
+		JButton NewMSGB = new JButton("Create Message");
+		JTextField searchField = new JTextField();
+		JButton searchButton = new JButton("Search");
 	}
 	public static void main(String[] args)
 	{
